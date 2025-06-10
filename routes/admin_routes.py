78a25a6f7e73f73156c31f6da37ecc5890fbaf4d
mwael <mwael.y25@gmail.com>
2025-06-10 19:56:57 +0300

    solved problem of add-admin route

--- routes/admin_routes.py
+++ routes/admin_routes.py
@@ -3,17 +3,26 @@ from sqlalchemy.orm import Session
 from controllers.admin_controller import add_admin_controller, get_admins_controller, regiadmin
 from models.schemas import Admin, AdminDBBase, AdminResponse
 from models.database import get_db
+from services.auth_service import get_current_admin
 
 router = APIRouter()
 
 @router.post("/add-admin", response_model=AdminDBBase)
-async def add_admin_route(admin_data: Admin, db: Session = Depends(get_db)):
-    return await add_admin_controller(admin_data, db=db)
+async def add_admin_route(
+    admin_data: Admin,
+    db: Session = Depends(get_db),
+    cur_admin=Depends(get_current_admin)
+):
+    return await add_admin_controller(admin_data, db=db, cur_admin=cur_admin)
 
 @router.get("/admins/{degree}", response_model=list[AdminResponse])
 @router.get("/admins/", response_model=list[AdminResponse])
-async def get_admins_route(degree: str | None = None, db: Session = Depends(get_db)):
-    return await get_admins_controller(degree, db=db)
+async def get_admins_route(
+    degree: str | None = None,
+    db: Session = Depends(get_db),
+    cur_admin=Depends(get_current_admin)
+):
+    return await get_admins_controller(degree, db=db, cur_admin=cur_admin)
 
 @router.post("/register-admin", response_model=AdminDBBase)
 async def register_master(admin_data: Admin, db: Session = Depends(get_db)):
