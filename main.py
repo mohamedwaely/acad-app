3d250da9d967a6e236754bb479f6459ce8c0cd39
mwael <mwael.y25@gmail.com>
2025-06-11 10:53:55 +0300

    change port

--- main.py
+++ main.py
@@ -47,4 +47,5 @@ async def root():
 app.include_router(router)
 
 if __name__=="__main__":
-    uvicorn.run(app=app, host="0.0.0.0", port=5555)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app=app, host="0.0.0.0", port=port)
